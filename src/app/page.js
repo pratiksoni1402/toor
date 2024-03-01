ada0fd260a9481a5b1e6569e50c8faf02c5f762a
Pratik <pratiksoni1402@gmail.com>
2024-03-01 18:41:41 +0530

    initial setup complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
-import Image from "next/image";
-
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
+export const revalidate = 0;
 export default function Home() {
   return (
     <div className="homepage">
