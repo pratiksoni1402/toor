2e90471dd29baae58793355a9cf21003db5412e1
Pratik <pratiksoni1402@gmail.com>
2024-03-12 12:49:34 +0530

    implementing session on some pages

---
+++ src/lib/session.js
@@ -0,0 +1,18 @@
+import { getIronSession } from "iron-session";
+import { cookies } from "next/headers";
+
+export async function getSession(){
+  const session = await getIronSession(cookies(), {password: process.env.SESSION_PASSWORD, cookieName:"ss-jewels"});
+  return session;
+}
+
+export async function getSessionId(){
+  const session = await getSession();
+  if(!session.id){
+    const newSessionId = crypto.randomUUID();
+    session.id = newSessionId;
+    await session.save();
+    return newSessionId;
+  }
+  return session.id;
+}
