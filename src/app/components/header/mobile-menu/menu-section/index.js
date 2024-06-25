60f6b031b638826377efb409eab81d84326bf1cb
Pratik <pratiksoni1402@gmail.com>
2024-06-25 18:54:21 +0530

    added section in mobile menu

--- src/app/components/header/mobile-menu/menu-section/index.js
+++ src/app/components/header/mobile-menu/menu-section/index.js
@@ -30,6 +30,7 @@ export default function TabList() {
               <TabsTrigger className="tablistvalue" value="rings">Rings</TabsTrigger>
               <TabsTrigger className="tablistvalue" value="earrings">Earrings</TabsTrigger>
               <TabsTrigger className="tablistvalue" value="bracelets">Bracelets</TabsTrigger>
+              <TabsTrigger className="tablistvalue" value="jewelry-pure-metals">Jewelry and Pure Metals</TabsTrigger>
             </TabsList>
           ) : (
             ''
@@ -429,7 +430,143 @@ export default function TabList() {
               </div>
             </div>
           </TabsContent>
+          <TabsContent value="jewelry-pure-metals">
+            <div className="tab-content h-full">
+              <div className=' flex justify-between border-b border-secondary'>
+                <button onClick={() => setActiveTab(null)} className='text-base font-roboto text-primary hover:font-semibold hover:underline'>
+                  <MoveLeft />
+                </button>
+                <h1 className='ml-[-30px] text-3xl text-center font-crimson text-primary'>Jewelry and Pure Metals</h1>
+                <div></div>
+              </div>
+              <div className='product-menu'>
+                <div className='grid grid-cols-2 gap-5'>
 
+                  <div className='col'>
+                    <div className='shop-by-style'>
+                      <h3 className='text-base text-secondary font-semibold pt-2'>Pure Metals</h3>
+                      <ul>
+                        <li><Link href='#' className='text-base text-accent font-roboto hover:text-primary hover:font-semibold'>Gold Bars</Link></li>
+                        <li><Link href='#' className='text-base text-accent font-roboto hover:text-primary hover:font-semibold'>Gold Coins</Link></li>
+                        <li><Link href='#' className='text-base text-accent font-roboto hover:text-primary hover:font-semibold'>Silver Bars</Link></li>
+                        <li><Link href='#' className='text-base text-accent font-roboto hover:text-primary hover:font-semibold'>Silver Coins</Link></li>
+
+                      </ul>
+                    </div>
+                  </div>
+
+                  <div className='col'>
+                    <div className='shop-by-style'>
+                      <h3 className='text-base text-secondary font-semibold pt-2'>Silver Sets</h3>
+                      <ul>
+                        <li className="product-list">
+                          <Link href='/platinum' className="flex items-center text-base text-accent font-roboto hover:text-primary hover:font-semibold">
+                            <span className="pl-1">Thali</span>
+                          </Link>
+                        </li>
+
+                        <li className="product-list py-1">
+                          <Link href='/yellow-gold' className="flex items-center text-base text-accent font-roboto hover:text-primary hover:font-semibold">
+                            <span className="pl-1">Glass Sets</span>
+                          </Link>
+                        </li>
+
+                        <li className="product-list">
+                          <Link href='/rose-gold' className="flex items-center text-base text-accent font-roboto hover:text-primary hover:font-semibold">
+                            <span className="pl-1">Thali and Bowl Sets</span>
+                          </Link>
+                        </li>
+
+                        <li className="product-list">
+                          <Link href='/rose-gold' className="flex items-center text-base text-accent font-roboto hover:text-primary hover:font-semibold">
+                            <span className="pl-1">Glass and Jug Sets</span>
+                          </Link>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+
+                  <div className='col'>
+                    <div className='shop-by-style'>
+                      <h3 className='text-base text-secondary font-semibold pt-2'>Gifts</h3>
+                      <ul>
+                        <li><Link href='#' className='text-base text-accent font-roboto hover:text-primary hover:font-semibold'>For Him</Link></li>
+                        <li><Link href='#' className='text-base text-accent font-roboto hover:text-primary hover:font-semibold'>For Her</Link></li>
+                        <li><Link href='#' className='text-base text-accent font-roboto hover:text-primary hover:font-semibold'>For Kids</Link></li>
+
+
+                      </ul>
+                    </div>
+                  </div>
+
+                  <div className='col'>
+                  <div className='shop-by-style'>
+                      <h3 className='text-base text-secondary font-semibold pt-2'>Accessories</h3>
+                      <ul>
+                        <li><Link href='#' className='text-base text-accent font-roboto hover:text-primary hover:font-semibold'>Nose Pins</Link></li>
+                        <li><Link href='#' className='text-base text-accent font-roboto hover:text-primary hover:font-semibold'>Anklets</Link></li>
+                        <li><Link href='#' className='text-base text-accent font-roboto hover:text-primary hover:font-semibold'>Evil Eyes</Link></li>
+                        <li><Link href='#' className='text-base text-accent font-roboto hover:text-primary hover:font-semibold'>Pendants</Link></li>
+                      </ul>
+                    </div>
+                  </div>
+
+                  <div className='col'>
+                    <h2 className="inner-heading text-base text-secondary font-semibold pt-2">Gemstone Bracelets</h2>
+                    <ul className="product-list-wrapper">
+
+                      <li className="product-list" >
+                        <Link href='/yellow-gold' className="flex items-center text-base text-accent font-roboto hover:text-primary hover:font-semibold">
+                          <span>
+                            <Image src='/uploads/images/swatch/blue-sapphire.svg' alt="Platinum" width={20} height={20} />
+                          </span>
+                          <span className="pl-1">Blue Sapphire</span>
+                        </Link>
+                      </li>
+
+                      <li className="product-list">
+                        <Link href='/rose-gold' className="flex items-center text-base text-accent font-roboto hover:text-primary hover:font-semibold">
+                          <span>
+                            <Image src='/uploads/images/swatch/emerald.svg' alt="Platinum" width={20} height={20} />
+                          </span>
+                          <span className="pl-1">Emerald </span>
+                        </Link>
+                      </li>
+
+                      <li className="product-list">
+                        <Link href='/silver' className="flex items-center text-base text-accent font-roboto hover:text-primary hover:font-semibold">
+                          <span>
+                            <Image src='/uploads/images/swatch/aquamarine.svg' alt="Platinum" width={20} height={20} />
+                          </span>
+                          <span className="pl-1">Aquamarine </span>
+                        </Link>
+                      </li>
+
+                      <li className="product-list">
+                        <Link href='/silver' className="flex items-center text-base text-accent font-roboto hover:text-primary hover:font-semibold">
+                          <span>
+                            <Image src='/uploads/images/swatch/morganite.svg' alt="Platinum" width={20} height={20} />
+                          </span>
+                          <span className="pl-1">Morganite</span>
+                        </Link>
+                      </li>
+
+                      <li className="product-list">
+                        <Link href='/silver' className="flex items-center text-base text-accent font-roboto hover:text-primary hover:font-semibold">
+                          <span>
+                            <Image src='/uploads/images/swatch/ruby.svg' alt="Platinum" width={20} height={20} />
+                          </span>
+                          <span className="pl-1">Ruby</span>
+                        </Link>
+                      </li>
+
+                    </ul>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </TabsContent>
         </Tabs>
       </div>
     </div>
